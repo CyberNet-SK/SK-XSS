61d7f4fe2d867c930efe2098c459b5b7c8b74490
Cyber Net <ssk717747@gmail.com>
2026-08-09 06:50:18 +0600

    Refactor log methods for improved formatting

--- lib/helper/log.py
+++ lib/helper/log.py
@@ -1,21 +1,33 @@
 '''
-PwnXSS - 2019/2020
-This project was created by Andripwn with Pwn0sec team. 
-Copyright under the MIT license
+SK XSS - Log Module
+Developer: Sheikh Sabbir
+Version: 1.0 Final
 '''
-from lib.helper.helper import * 
 from datetime import datetime
+from lib.helper.helper import *
+
 class Log:
 
-	@classmethod
-	def info(self,text):
- 		print("["+Y+datetime.now().strftime("%H:%M:%S")+N+"] ["+G+"INFO"+N+"] "+text)
- 
-	@classmethod
-	def warning(self,text):
-		print("["+Y+datetime.now().strftime("%H:%M:%S")+N+"] ["+Y+"WARNING"+N+"] "+text)
+    @classmethod
+    def info(self, text):
+        print("[" + Y + datetime.now().strftime("%H:%M:%S") + N + "] [" + G + "INFO" + N + "] " + text)
 
-	@classmethod
-	def high(self,text):
- 		print("["+Y+datetime.now().strftime("%H:%M:%S")+N+"] ["+R+"CRITICAL"+N+"] "+text)
- 		
+    @classmethod
+    def warning(self, text):
+        print("[" + Y + datetime.now().strftime("%H:%M:%S") + N + "] [" + Y + "WARNING" + N + "] " + text)
+
+    @classmethod
+    def high(self, text):
+        print("[" + Y + datetime.now().strftime("%H:%M:%S") + N + "] [" + R + "CRITICAL" + N + "] " + text)
+
+    @classmethod
+    def error(self, text):
+        print("[" + Y + datetime.now().strftime("%H:%M:%S") + N + "] [" + R + "ERROR" + N + "] " + text)
+
+    @classmethod
+    def success(self, text):
+        print("[" + Y + datetime.now().strftime("%H:%M:%S") + N + "] [" + G + "SUCCESS" + N + "] " + text)
+
+    @classmethod
+    def debug(self, text):
+        print("[" + Y + datetime.now().strftime("%H:%M:%S") + N + "] [" + C + "DEBUG" + N + "] " + text)
